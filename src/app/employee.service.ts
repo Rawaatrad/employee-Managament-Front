dad0c5051366d44d31ccf0208a81104864c5c800
rawaa <rawaatrad@yahoo.com>
2021-10-10 13:58:41 +0100

    connecting Angular with List Employee REST API

---
+++ src/app/employee.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
+
+@Injectable({ //it can be injected in various components
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  private Url: string = 'http://localhost:8080/api/v1/employees'
+
+  constructor(private httpClient: HttpClient) { }
+
+  getEmployeesList(): Observable<Employee[]> {
+    return this.httpClient.get<Employee[]>(`${this.Url}`);
+
+  }
+}
---
+++ src/app/employee.ts
@@ -0,0 +1,9 @@
+export class Employee {
+  constructor(
+
+    public id?: Number,
+    public firstName?: String,
+    public lastName?: String
+
+  ) { }
+}
